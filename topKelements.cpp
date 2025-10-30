8818639c3a2318300c9d242b02af1ea9b068b701
aadyasinghh26 <aadyasingh910@gmail.com>
2025-10-30 23:29:13 +0530

    Add files via upload

---
+++ topKelements.cpp
@@ -0,0 +1,49 @@
+#include<iostream>  //jab -1 aagaya tab tak ke top k elements print karwane h
+#include<queue>
+using namespace std;
+
+void printHeap(priority_queue<int, vector<int>, greater<int> >h)
+{
+    while (!h.empty())
+    {
+        cout<<h.top()<<" ";
+        h.pop();
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    priority_queue<int, vector<int>, greater<int> >h;   //min heap
+    int k=3;
+    int count=0;
+
+    while (1)
+    {
+        int data;
+        cin>>data;
+    
+
+    if (data==-1)
+    {
+        printHeap(h);
+        continue;
+    }
+
+    if (count<k)
+    {
+        h.push(data);
+        count++;
+    }
+    else
+    {
+        if (h.top()<data)
+        {
+            h.pop();
+            h.push(data);
+        }
+    }
+}
+
+    return 0;
+}
